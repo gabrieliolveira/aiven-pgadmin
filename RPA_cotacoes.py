4dc6b36390b322d91ab2fd51c04f6c08deeda83d
Gabrieli Oliveira <gapettena@gmail.com>
2024-08-30 08:12:36 -0300

    fixing code

--- RPA_cotacoes.py
+++ RPA_cotacoes.py
@@ -1,3 +1,4 @@
+import datetime
 import time
 from selenium.webdriver.chrome.options import Options
 import psycopg2
@@ -20,18 +21,20 @@ time.sleep(2)
 dolar = driver.find_element(By.XPATH, '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').text
 dolar = dolar.replace(',', '.')
 float(dolar)
+current_date = datetime.datetime.now().date() 
+current_time = datetime.datetime.now().time()
 
 driver.close()
 
 conexao = psycopg2.connect(
-        dbname='dbcotacoes', 
-        user='avnadmin', 
-        password='AVNS_DgQT5YHAmGpK5QE600Y', 
-        host='pg-13f76388-gapettena-cecb.g.aivencloud.com', 
-        port='21452'
+        dbname='dbcotacoes_xqci', 
+        user='postgresss', 
+        password='WHRyNdc6SxGCU1xqN6AY9oaSVJXM0270', 
+        host='dpg-cr8q71l6l47c73bncnb0-a.oregon-postgres.render.com', 
+        port='5432'
     )
 cursor = conexao.cursor()
-cursor.execute(f'CALL inserir_cotacao({dolar})')
+cursor.execute("CALL inserir_cotacao_dolar(%s, %s, %s);", (current_date, current_time, dolar))
 conexao.commit()
 
 cursor.close()
